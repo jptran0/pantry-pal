42739d4abc98bce576c18f54c08cb95d0c19fc27
Jazmine <117502378+jptran0@users.noreply.github.com>
2023-05-17 17:40:05 +0000

    added recipes table

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Recipe < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_172114) do
+ActiveRecord::Schema.define(version: 2023_05_17_173951) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2023_05_17_172114) do
     t.string "item"
     t.integer "category_id"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
